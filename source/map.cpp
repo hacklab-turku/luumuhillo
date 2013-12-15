9c9f8c99c26427f50db9c81c30b4eeffa9696861
Zidit <mika.terhokoski@gmail.com>
2013-12-15 05:24:45 +0200

    fixed possible overflows in map code

--- source/map.cpp
+++ source/map.cpp
@@ -155,7 +155,7 @@ void map::genBoulders(int x, int y)	//generate a wall of boulders
 
 bool map::isSolid(int x, int y)
 {
-	if (x < 0 || x > MAP_SIZE_X || y < 0 || y > MAP_SIZE_Y)
+	if (x < 0 || x >= MAP_SIZE_X || y < 0 || y >= MAP_SIZE_Y)
 		return true;
 	if(world[x][y]==BOULDER) return true; 
 	if(world[x][y]==PINE_TREE) return true;
@@ -165,7 +165,7 @@ bool map::isSolid(int x, int y)
 
 char map::getCellData(int x, int y)
 {
-	if (x < 0 || x > MAP_SIZE_X || y < 0 || y > MAP_SIZE_Y)
+	if (x < 0 || x >= MAP_SIZE_X || y < 0 || y >= MAP_SIZE_Y)
 		return 'E'; //Off edge
 	return world[x][y];
 
